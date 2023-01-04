63c6f00a0c1e7d3e5f9e3f3f94551d016783db3a
krishnaprasad2098 <payamma.krishna@gmail.com>
2023-01-04 22:17:40 +0530

    commit

--- src/components/SearchExercise.js
+++ src/components/SearchExercise.js
@@ -1,10 +1,18 @@
 import React, { useEffect, useState } from "react";
 const SearchExercise = () => {
-  const[Search,setSearch]=useState("");
-  const handlesearch= (e)=>{
-    setSearch(e.target.value.toLowerCase());
+  
+    const[search,setSearch] = useState("");
+    const Search=(e)=>{
+      setSearch(e.target.value.toLowerCase());
+    }
 
-  }
+    const handleSearch = async () =>{
+      if(search){
+         
+      }
+
+    }
+ 
   
   return (
     <div>
@@ -13,8 +21,9 @@ const SearchExercise = () => {
       {/* <h1 className=" text-center  mt-4 text-4xl text-amber-800 font-serif font-bold capitalize">--Napoleon Hill</h1> */}
       <p className="text-center p-4 text-xl text-teal-600 font-extrabold font-mono">Know Your Exercises</p>
       <div className="flex justify-center">
-      <input type="text" placeholder="Enter Exercise" className="w-6/12 placeholder-gray-700 outline-none"></input>
-      <button type="submit" className=" w-20 outline-none p-1 rounded-r bg-slate-700 text-white  lg:bg-red-500">Search</button>
+      <input type="text" placeholder="Enter Exercise" className="w-6/12 placeholder-gray-700 outline-none" 
+      value={search} onChange={Search}></input>
+      <button type="submit" onClick={handleSearch} className=" w-20 outline-none p-1 rounded-r bg-slate-700 text-white  lg:bg-red-500">Search</button>
       </div>
     </div>
   ); 
