aa04c712976f09ed2a3aaa3be470330b03bd0dec
krishnaprasad2098 <payamma.krishna@gmail.com>
2023-01-22 18:12:17 +0530

    small changes in file structure

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,14 @@
-import React, {useState} from 'react'
-import Exercise from '../components/Exercise'
+import React from 'react'
+import AboutUs from '../components/AboutUs'
 import MainBanner from '../components/MainBanner'
 import SearchExercise from '../components/SearchExercise'
-// import SearchBar from '../components/SearchBar'
+
 const Home = () => {
-  // const[bodyPart,setbodyParts] = useState('all')
   return (
     <div>
       <MainBanner />
       <SearchExercise />
-      <Exercise />
+      <AboutUs />
     </div>
   )
 }
