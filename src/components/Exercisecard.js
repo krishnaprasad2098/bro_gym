020c4a70f34a2beb736b8e8d62a48f2a49ce2532
krishnaprasad2098 <payamma.krishna@gmail.com>
2023-01-22 10:11:03 +0530

    reworked on exercisecard

--- src/components/Exercisecard.js
+++ src/components/Exercisecard.js
@@ -2,8 +2,8 @@ import React from 'react'
 import exerciseimg from '../assets/images/image-1.png'
 const Exercisecard = () => {
   return (
-    <div className=" lg: ml-8 flex justify-center items-center gap-4 " >
-        <div className="h-auto lg: border-green-700 w-80 border-4 h-96">
+    <div className=" grid grid-col-2 lg:flex justify-center items-center gap-2 m-2" >
+        <div className="lg: border-green-700 w-80 border-4 h-auto">
             <img src={exerciseimg} alt="" className="h-32 flex ml-auto mr-auto"></img>
             <h1 className="text-center">Excercise</h1>
             <p className="p-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, non ipsam! Odio, doloremque porro modi 
@@ -11,7 +11,7 @@ const Exercisecard = () => {
             sunt inventore.</p>
         </div>
 
-        <div className=" h-auto lg: border-green-700 w-80 border-4 h-96">
+        <div className="   lg: border-green-700 w-80 border-4 h-auto">
             <img src={exerciseimg} alt="" className="h-32 flex ml-auto mr-auto"></img>
             <h1 className="text-center">Excercise</h1>
             <p className="p-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, non ipsam! Odio, doloremque porro modi 
@@ -19,7 +19,7 @@ const Exercisecard = () => {
             sunt inventore.</p>
         </div>
 
-        <div className="h-auto lg: border-green-700 w-80 border-4 h-96">
+        <div className="lg: border-green-700 w-80 border-4 h-auto">
             <img src={exerciseimg} alt="" className="h-32 flex ml-auto mr-auto"></img>
             <h1 className="text-center">Excercise</h1>
             <p className="p-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, non ipsam! Odio, doloremque porro modi 
@@ -27,7 +27,7 @@ const Exercisecard = () => {
             sunt inventore.</p>
         </div>
 
-        <div className="h-auto lg: border-green-700 w-80 border-4 h-96">
+        <div className=" lg: border-green-700 w-80 border-4 h-auto">
             <img src={exerciseimg} alt="" className="h-32 flex ml-auto mr-auto"></img>
             <h1 className="text-center">Excercise</h1>
             <p className="p-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, non ipsam! Odio, doloremque porro modi 
@@ -35,7 +35,7 @@ const Exercisecard = () => {
             sunt inventore.</p>
         </div>
 
-        <div className="h-auto lg: border-green-700 w-80 border-4 h-96">
+        <div className=" lg: border-green-700 w-80 border-4 h-auto">
             <img src={exerciseimg} alt="" className="h-32 flex ml-auto mr-auto"></img>
             <h1 className="text-center">Excercise</h1>
             <p className="p-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, non ipsam! Odio, doloremque porro modi 
