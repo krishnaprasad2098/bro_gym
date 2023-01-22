d2be9bd5c63e712355a5f3d411d6cc96e56763cd
krishnaprasad2098 <payamma.krishna@gmail.com>
2023-01-22 22:45:39 +0530

    started about us

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,10 +1,14 @@
 import React from 'react'
 
-const Exercise = () => {
+const Aboutus = () => {
   return (
-    <div className="">
+    <div className="pt-4">
+      <h1 className="text-3xl text-teal-900 font-extrabold text-center">Get Know About Us</h1>
+      <div>
+        <img src={} alt="" className=""></img>
+      </div>
     </div>
   )
 }
 
-export default Exercise
+export default Aboutus
