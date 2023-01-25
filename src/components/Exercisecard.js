b7ed79c30970e21ce12d9f6043e685e12b3e68bb
krishnaprasad2098 <payamma.krishna@gmail.com>
2023-01-25 19:08:27 +0530

    restructured styling

--- src/components/Exercisecard.js
+++ src/components/Exercisecard.js
@@ -5,8 +5,8 @@ const Exercisecard = () => {
 
     // flex justify-center items-center
 
-    <div className="grid md:grid grid-cols-2 gap-3 m-2" > 
-        <div className="w-auto lg: border-green-700 w-auto border-4">
+    <div className=" md:grid grid-cols-3 gap-3 m-2 " > 
+        <div className="w-auto lg: border-green-700 w-auto border-4 mb-2">
             <img src={exerciseimg} alt="" className="h-32  ml-auto mr-auto"></img>
             <h1 className="text-center">Excercise</h1>
             <p className="p-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, non ipsam! Odio, doloremque porro modi 
@@ -14,7 +14,7 @@ const Exercisecard = () => {
             sunt inventore.</p>
         </div>
 
-        <div className="w-auto lg: border-green-700 w-auto border-4 h-auto">
+        <div className="w-auto lg: border-green-700 w-auto border-4 h-auto mb-2">
             <img src={exerciseimg} alt="" className="h-32  ml-auto mr-auto"></img>
             <h1 className="text-center">Excercise</h1>
             <p className="p-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, non ipsam! Odio, doloremque porro modi 
@@ -22,7 +22,7 @@ const Exercisecard = () => {
             sunt inventore.</p>
         </div>
 
-        <div className="w-auto lg:  border-green-700 w-auto border-4 h-auto">
+        <div className="w-auto lg:  border-green-700 w-auto border-4 h-auto mb-2">
             <img src={exerciseimg} alt="" className="h-32  ml-auto mr-auto"></img>
             <h1 className="text-center">Excercise</h1>
             <p className="p-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, non ipsam! Odio, doloremque porro modi 
@@ -30,7 +30,7 @@ const Exercisecard = () => {
             sunt inventore.</p>
         </div>
 
-        <div className="w-auto lg:  border-green-700 w-auto border-4 h-auto">
+        <div className="w-auto lg:  border-green-700 w-auto border-4 h-auto mb-2">
             <img src={exerciseimg} alt="" className="h-32  ml-auto mr-auto"></img>
             <h1 className="text-center">Excercise</h1>
             <p className="p-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, non ipsam! Odio, doloremque porro modi 
@@ -38,7 +38,7 @@ const Exercisecard = () => {
             sunt inventore.</p>
         </div>
 
-        <div className="w-auto lg: border-green-700 w-auto border-4 h-auto">
+        <div className="w-auto lg: border-green-700 w-auto border-4 h-auto mb-2">
             <img src={exerciseimg} alt="" className="h-32  ml-auto mr-auto"></img>
             <h1 className="text-center">Excercise</h1>
             <p className="p-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, non ipsam! Odio, doloremque porro modi 
@@ -46,7 +46,7 @@ const Exercisecard = () => {
             sunt inventore.</p>
         </div>
 
-        <div className="w-auto lg: border-green-700 w-auto border-4 h-auto">
+        <div className="w-auto lg: border-green-700 w-auto border-4 h-auto mb-2">
             <img src={exerciseimg} alt="" className="h-32  ml-auto mr-auto"></img>
             <h1 className="text-center">Excercise</h1>
             <p className="p-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, non ipsam! Odio, doloremque porro modi 
--- src/components/PriceCard.js
+++ src/components/PriceCard.js
@@ -2,11 +2,11 @@ import React from 'react'
 import pricedetails from '../data/Pricedetails.js'
 const PriceCard = () => {
   return (
-    <div className="grid grid-cols-4 gap-4 mt-4">
+    <div className="md:grid grid-cols-3 gap-4 mt-4 sm:grid grid-cols-2">
 
      {pricedetails.map((value)=>{
       return(
-        <div className="border border-green-800 w-auto gap-4 text-center">
+        <div className="border border-green-800 w-34 gap-4 text-center">
           <img src={value.imagelink} alt="image" className="w-auto h-32 ml-auto mr-auto "/>
           <h1>{value.name}</h1>
           <p>{value.description}</p>
