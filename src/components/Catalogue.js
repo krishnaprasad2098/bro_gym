a72cbaa28ab369758ddfdce11b22680392a6c64a
krishnaprasad2098 <payamma.krishna@gmail.com>
2023-02-11 21:57:49 +0530

    refactored the code and made some samall chanes.

---
+++ src/components/Catalogue.js
@@ -0,0 +1,16 @@
+import React from "react";
+import aboutus from "../assets/images/aboutus.jpg";
+import PriceCard from "./PriceCard.js";
+const Aboutus = () => {
+  return (
+    <div className="">
+      <h1 className="font-medium text-center mt-4 text-2xl text-emerald-900 p-2">
+        "COME ON DIVE INTO OUR CATALOGUE"
+      </h1>
+
+      <PriceCard />
+    </div>
+  );
+};
+
+export default Aboutus;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,16 @@
-import React from 'react'
-import AboutUs from '../components/AboutUs'
-import MainBanner from '../components/MainBanner'
-import SearchExercise from '../components/SearchExercise'
+import React from "react";
+import Catalogue from "../components/Catalogue.js";
+import MainBanner from "../components/MainBanner";
+import SearchExercise from "../components/SearchExercise";
 
 const Home = () => {
   return (
     <div>
       <MainBanner />
       <SearchExercise />
-      <AboutUs />
+      <Catalogue />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
