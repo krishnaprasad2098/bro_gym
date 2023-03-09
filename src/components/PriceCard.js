cfef1d62f3c581dbccaf316eb5a247cc4d4e6021
krishnaprasad2098 <payamma.krishna@gmail.com>
2023-03-09 20:18:50 +0530

    updated the styling to the pricecard.

--- src/components/PriceCard.js
+++ src/components/PriceCard.js
@@ -1,22 +1,28 @@
-import React from 'react'
-import pricedetails from '../data/Pricedetails.js'
+import React from "react";
+import pricedetails from "../data/Pricedetails.js";
 const PriceCard = () => {
   return (
-    <div className="md:grid grid-cols-4 gap-3 mt-1 mr-2 ml-2 ">
-
-     {pricedetails.map((value)=>{
-      return(
-        <div className="border-4 border-red-800 w-34 gap-4 text-center mb-1">
-          <img src={value.imagelink} alt="image_" className="w-auto h-32 ml-auto mr-auto mt-1 "/>
-          <h1 className="font-medium text-2xl">{value.name}</h1>
-          <p className="font-semibold">{value.description}</p>
-          <h1 className="bg-orange-500 text-xl text-white" >{value.price}</h1>
-        </div>
-      )
-     })}
-
+    <div className="md:grid grid-cols-3  gap-3 mt-1 mx-2 py-4 ">
+      {pricedetails.map((value) => {
+        return (
+          <div className="border-2 border-red-800 w-34 gap-y-8 text-center mb-1 rounded-xl p-2">
+            <img
+              src={value.imagelink}
+              alt="image_"
+              className="w-auto h-32 ml-auto mr-auto mt-1 mix-blend-multiply "
+            />
+            <h1 className="font-bold font-sans uppercase text-2xl">
+              {value.name}
+            </h1>
+            <p className="font-semibold py-2">{value.description}</p>
+            <h1 className="bg-orange-500 text-xl  text-white rounded-lg py-2 hover:bg-orange-300">
+              {value.price}
+            </h1>
+          </div>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default PriceCard
+export default PriceCard;
--- src/data/Pricedetails.js
+++ src/data/Pricedetails.js
@@ -1,64 +1,68 @@
-import logo1 from '../assets/images/image-1.png'
+import logo1 from "../assets/images/image-1.png";
 
-import logo2 from '../assets/images/image-2.jpg'
-import logo3 from '../assets/images/image-3.png'
-import logo4 from '../assets/images/image-4.png'
-import logo5 from '../assets/images/image-5.png'
-import logo6 from '../assets/images/image-6.png'
-
-
+import logo2 from "../assets/images/image-2.jpg";
+import logo3 from "../assets/images/image-3.png";
+import logo4 from "../assets/images/image-4.png";
+import logo5 from "../assets/images/image-5.png";
+import logo6 from "../assets/images/image-6.png";
 
 const pricedetails = [
-    {
-        "id":1,
-        "imagelink":logo1,
-        "name":"trainer",
-        "price":'Rs.220',
-        "description":'lorem ipsum dolar init polar ghikjd drdf'
-    },
-    {
-        "id":2,
-        "imagelink":logo2,
-        "name":"trainer",
-        "price":'Rs.200',
-        "description":'lorem ipsum dolar init polar ghikjd drdf'
-    },
-    {
-        "id":3,
-        "imagelink":logo3,
-        "name":"trainer",
-        "price":'Rs.300',
-        "description":'lorem ipsum dolar init polar ghikjd drdf'
-    },
-    {
-        "id":4,
-        "imagelink":logo4,
-        "name":"trainer",
-        "price":'Rs.300',
-        "description":'lorem ipsum dolar init polar ghikjd drdf'
-    },
-    {
-        "id":5,
-        "imagelink":logo5,
-        "name":"trainer",
-        "price":'Rs.300',
-        "description":'lorem ipsum dolar init polar ghikjd drdf'
-    },
-    {
-        "id":6,
-        "imagelink":logo6,
-        "name":"trainer",
-        "price":'Rs.300',
-        "description":'lorem ipsum dolar init polar ghikjd drdf'
-    },
-    {
-        "id":7,
-        "imagelink":logo6,
-        "name":"trainer",
-        "price":'Rs.300',
-        "description":'lorem ipsum dolar init polar ghikjd drdf '
-    }
+  {
+    id: 1,
+    imagelink: logo1,
+    name: "trainer",
+    price: "Rs.220",
+    description:
+      "lorem ipsum dolar init polar ghikjd drdf lorem ipsum dolar init polar ghikjd drdf lorem ipsum dolar init polar ghikjd drdf",
+  },
+  {
+    id: 2,
+    imagelink: logo2,
+    name: "trainer",
+    price: "Rs.200",
+    description:
+      "lorem ipsum dolar init polar ghikjd drdf lorem ipsum dolar init polar ghikjd drdf lorem ipsum dolar init polar ghikjd drdf",
+  },
+  {
+    id: 3,
+    imagelink: logo3,
+    name: "trainer",
+    price: "Rs.300",
+    description:
+      "lorem ipsum dolar init polar ghikjd drdf lorem ipsum dolar init polar ghikjd drdf lorem ipsum dolar init polar ghikjd drdf",
+  },
+  {
+    id: 4,
+    imagelink: logo4,
+    name: "trainer",
+    price: "Rs.300",
+    description:
+      "lorem ipsum dolar init polar ghikjd drdf lorem ipsum dolar init polar ghikjd drdf lorem ipsum dolar init polar ghikjd drdf",
+  },
+  {
+    id: 5,
+    imagelink: logo5,
+    name: "trainer",
+    price: "Rs.300",
+    description:
+      "lorem ipsum dolar init polar ghikjd drdf lorem ipsum dolar init polar ghikjd drdf lorem ipsum dolar init polar ghikjd drdf",
+  },
+  {
+    id: 6,
+    imagelink: logo6,
+    name: "trainer",
+    price: "Rs.300",
+    description:
+      "lorem ipsum dolar init polar ghikjd drdf lorem ipsum dolar init polar ghikjd drdf lorem ipsum dolar init polar ghikjd drdf",
+  },
+  {
+    id: 7,
+    imagelink: logo6,
+    name: "trainer",
+    price: "Rs.300",
+    description:
+      "lorem ipsum dolar init polar ghikjd drdf lorem ipsum dolar init polar ghikjd drdf lorem ipsum dolar init polar ghikjd drdf ",
+  },
+];
 
-]
-
-export default pricedetails
+export default pricedetails;
