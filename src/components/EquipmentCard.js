9e73efa4088b8e3eae092bc0fefbc3d5e616610a
krishnaprasad2098 <payamma.krishna@gmail.com>
2023-03-26 17:47:52 +0530

    added padding

--- src/components/EquipmentCard.js
+++ src/components/EquipmentCard.js
@@ -1,58 +1,83 @@
-import React from 'react'
-import exerciseimg from '../assets/images/image-2.jpg'
+import React from "react";
+import exerciseimg from "../assets/images/image-2.jpg";
 const EquipmentCard = () => {
   return (
-    <div className="md:grid grid-cols-3 gap-3 m-2 " > 
-        <div className="w-auto lg: border-green-700 w-auto border-4 mb-2">
-            <img src={exerciseimg} alt="" className="h-32  ml-auto mr-auto"></img>
-            <h1 className="text-center">Excercise</h1>
-            <p className="p-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, non ipsam! Odio, doloremque porro modi 
-            eos natus possimus hic aspernatur rerum quos architecto necessitatibus consequuntur cupiditate commodi impedit
-            sunt inventore.</p>
-        </div>
+    <div className="md:grid grid-cols-3 gap-3 m-2 ">
+      <div className=" lg: border-green-700 w-auto border-4 mb-2">
+        <img
+          src={exerciseimg}
+          alt=""
+          className="h-32  ml-auto mr-auto py-2"
+        ></img>
+        <h1 className="text-center">Excercise</h1>
+        <p className="p-2">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, non
+          ipsam! Odio, doloremque porro modi eos natus possimus hic aspernatur
+          rerum quos architecto necessitatibus consequuntur cupiditate commodi
+          impedit sunt inventore.
+        </p>
+      </div>
 
-        <div className="w-auto lg: border-green-700 w-auto border-4 h-auto mb-2">
-            <img src={exerciseimg} alt="" className="h-32  ml-auto mr-auto"></img>
-            <h1 className="text-center">Excercise</h1>
-            <p className="p-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, non ipsam! Odio, doloremque porro modi 
-            eos natus possimus hic aspernatur rerum quos architecto necessitatibus consequuntur cupiditate commodi impedit
-            sunt inventore.</p>
-        </div>
+      <div className=" lg: border-green-700 w-auto border-4 h-auto mb-2">
+        <img src={exerciseimg} alt="" className="h-32  ml-auto mr-auto"></img>
+        <h1 className="text-center">Excercise</h1>
+        <p className="p-2">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, non
+          ipsam! Odio, doloremque porro modi eos natus possimus hic aspernatur
+          rerum quos architecto necessitatibus consequuntur cupiditate commodi
+          impedit sunt inventore.
+        </p>
+      </div>
 
-        <div className="w-auto lg:  border-green-700 w-auto border-4 h-auto mb-2">
-            <img src={exerciseimg} alt="" className="h-32  ml-auto mr-auto"></img>
-            <h1 className="text-center">Excercise</h1>
-            <p className="p-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, non ipsam! Odio, doloremque porro modi 
-            eos natus possimus hic aspernatur rerum quos architecto necessitatibus consequuntur cupiditate commodi impedit
-            sunt inventore.</p>
-        </div>
+      <div className=" lg:  border-green-700 w-auto border-4 h-auto mb-2">
+        <img src={exerciseimg} alt="" className="h-32  ml-auto mr-auto"></img>
+        <h1 className="text-center">Excercise</h1>
+        <p className="p-2">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, non
+          ipsam! Odio, doloremque porro modi eos natus possimus hic aspernatur
+          rerum quos architecto necessitatibus consequuntur cupiditate commodi
+          impedit sunt inventore.
+        </p>
+      </div>
 
-        <div className="w-auto lg:  border-green-700 w-auto border-4 h-auto mb-2">
-            <img src={exerciseimg} alt="" className="h-32  ml-auto mr-auto"></img>
-            <h1 className="text-center">Excercise</h1>
-            <p className="p-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, non ipsam! Odio, doloremque porro modi 
-            eos natus possimus hic aspernatur rerum quos architecto necessitatibus consequuntur cupiditate commodi impedit
-            sunt inventore.</p>
-        </div>
+      <div className=" lg:  border-green-700 w-auto border-4 h-auto mb-2">
+        <img src={exerciseimg} alt="" className="h-32  ml-auto mr-auto"></img>
+        <h1 className="text-center">Excercise</h1>
+        <p className="p-2">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, non
+          ipsam! Odio, doloremque porro modi eos natus possimus hic aspernatur
+          rerum quos architecto necessitatibus consequuntur cupiditate commodi
+          impedit sunt inventore.
+        </p>
+      </div>
 
-        <div className="w-auto lg: border-green-700 w-auto border-4 h-auto mb-2">
-            <img src={exerciseimg} alt="" className="h-32  ml-auto mr-auto"></img>
-            <h1 className="text-center">Excercise</h1>
-            <p className="p-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, non ipsam! Odio, doloremque porro modi 
-            eos natus possimus hic aspernatur rerum quos architecto necessitatibus consequuntur cupiditate commodi impedit
-            sunt inventore.</p>
-        </div>
+      <div className=" lg: border-green-700 w-auto border-4 h-auto mb-2">
+        <img src={exerciseimg} alt="" className="h-32  ml-auto mr-auto"></img>
+        <h1 className="text-center">Excercise</h1>
+        <p className="p-2">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, non
+          ipsam! Odio, doloremque porro modi eos natus possimus hic aspernatur
+          rerum quos architecto necessitatibus consequuntur cupiditate commodi
+          impedit sunt inventore.
+        </p>
+      </div>
 
-        <div className="w-auto lg: border-green-700 w-auto border-4 h-auto mb-2">
-            <img src={exerciseimg} alt="" className="h-32  ml-auto mr-auto"></img>
-            <h1 className="text-center">Excercise</h1>
-            <p className="p-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, non ipsam! Odio, doloremque porro modi 
-            eos natus possimus hic aspernatur rerum quos architecto necessitatibus consequuntur cupiditate commodi impedit
-            sunt inventore.</p>
-        </div>
-
+      <div className=" lg: border-green-700 w-auto border-4 h-auto mb-2">
+        <img
+          src={exerciseimg}
+          alt=""
+          className="h-32  ml-auto mr-auto py-2"
+        ></img>
+        <h1 className="text-center">Excercise</h1>
+        <p className="p-2">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, non
+          ipsam! Odio, doloremque porro modi eos natus possimus hic aspernatur
+          rerum quos architecto necessitatibus consequuntur cupiditate commodi
+          impedit sunt inventore.
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default EquipmentCard
+export default EquipmentCard;
