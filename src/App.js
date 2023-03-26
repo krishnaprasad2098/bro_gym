ddbbb0c24d9c5c2e77016968bf98385f6dbb297b
krishnaprasad2098 <payamma.krishna@gmail.com>
2023-03-26 17:49:20 +0530

    fixed the bugs.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react'
-import {Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import ExerciseDetail from './pages/ExerciseDetail';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import {BrowserRouter} from 'react-router-dom'
-
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import ExerciseDetail from "./pages/ExerciseDetail";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import { BrowserRouter } from "react-router-dom";
 
 const App = () => {
   return (
-    <BrowserRouter  basename='/bro_gym'>
+    <BrowserRouter basename="/bro_gym">
+      <div className="m-auto bg-slate-50">
+        <Navbar />
+        <Home />
+        {/* <ExerciseDetail /> */}
 
-    <div className="m-auto bg-slate-50">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/exercise" element={<ExerciseDetail />} /> {/*rendering dynamically inside a component */}
-      </Routes>
-      <Footer />
-    </div>
+        <Footer />
+      </div>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
